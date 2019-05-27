53d54352f843e21fe6940ff03dae3cf332c086a3
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2019-05-27 11:42:43 +0530

    Tokenization example in python

--- Tokenization.py
+++ Tokenization.py
@@ -5,3 +5,12 @@ doc = nlp(mystring)
 
 for token in doc:
   print(token.text, token.pos_, token.dep_)
+
+for entity in doc.ents:
+  print(entity, entity.label_)
+  print(str(spacy.explain(entity.label_)))
+  print('\n')
+  
+from spacy import displacy
+displacy.render(doc, style = 'dep', jupyter = True)
+displacy.render(doc, style = 'ent', jupyter = True)
