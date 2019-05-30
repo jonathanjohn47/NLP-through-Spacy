b19aaffb01c8d0e08291adb6d0df96f34a6d7d57
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2019-05-30 10:55:51 +0530

    Everything about Named Entity Recognition
    1. Listing all named entities in a doc
    2. Adding a word as a named entity
    3. Adding a phrase as a named entity
    4. Finding the frequency of a particular named entity (e.g. 'MONEY') in the document.

--- Named Entity Recognition.py
+++ Named Entity Recognition.py
@@ -28,3 +28,29 @@ new_ent = Span(doc,0,1,label=ORG)
 doc.ents = list(doc.ents) + [new_ent]
 
 show_ents(doc)
+
+#Adding a phrase as a new entity
+doc = nlp("Our company created a brand new vacuum cleaner." "This new vacuum-cleaner is the best in show")
+from spacy.matcher import PhraseMatcher
+matcher = PhraseMatcher(nlp.vocab)
+
+phrase_list = ['vacuum cleaner', 'vacuum-cleaner']
+
+phrase_patterns = [nlp(text) for text in phrase_list]
+
+matcher.add('newproduct', None, *phrase_patterns)
+
+found_matches = matcher(doc)
+found_matches
+
+from spacy.tokens import Span
+PROD = doc.vocab.strings['PRODUCT']
+
+new_ents = [Span(doc, match[1], match[2], label=PROD) for match in found_matches]
+doc.ents = list(doc.ents) + new_ents
+
+show_ents(doc)
+
+#finding how many times a named entity is present in the document
+doc = nlp("Originally I paid $29.95 for this car toy, but now it is marked down by 10 dollars.")
+[ent for ent in doc.ents if ent.label_ == 'MONEY']
