b89b271e6ea7924b6a1bd594f78652b2587f751a
Jonathan John <44868912+jonathanjohn47@users.noreply.github.com>
2019-05-30 10:56:13 +0530

    Delete add new NER.py

--- add new NER.py
+++
@@ -1,5 +0,0 @@
-from spacy.tokens import Span
-ORG = doc.vocab.strings[u"ORG"]
-
-new_ent = Span(doc,0,1,label=ORG) # 0 is the starting token in span of doc, 1 is the ending token (exclusive) in span of the doc.
-doc.ents = list(doc.ents) + [new_ent]
